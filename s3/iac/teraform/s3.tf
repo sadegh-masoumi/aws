48236702056ca1601a08215fddfab6fe96bcb68e
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2026-03-10 14:08:33 +0400

    Terraform ⚡️

---
+++ s3/iac/teraform/s3.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.35.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
